b008a78a8dabc91a2846d32893fbcb563099cb01
Dario_W <wasik.dariusz24@gmail.com>
2024-06-20 11:06:54 +0200

    put all operator event listeners into one, split equal symbol out off operators

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let operator;
 let plus = document.querySelector("#add");
 let equal = document.querySelector('#equal')
 let paraLow = document.querySelector(".lowerDisplay");
-let value = 0; 
+let value = 0;
 let minus = document.querySelector("#subtract");
 let multiplyBtn = document.querySelector('#multiply');
 let divideBtn = document.querySelector('#divide');
@@ -12,7 +12,7 @@ let clear = document.querySelector('#clear');
 let deleteBtn = document.querySelector('#delete');
 let numb = document.querySelectorAll('.numb');
 let buttons = document.querySelector("buttons");
-
+let operators = document.querySelectorAll(".operator");
 
 console.log(numb);
 function add (a, b) {
@@ -26,9 +26,9 @@ function add (a, b) {
  }
  function divide (a, b) {
      return a / b;
- }
-
- function operate(firstNumb, secundNumb, operator) {
+    }
+    
+    function operate(firstNumb, secundNumb, operator) {
      switch (operator) {
          case '+' :
              return add(firstNumb, secundNumb);
@@ -39,23 +39,49 @@ function add (a, b) {
          case '*' :
              return multiply(firstNumb, secundNumb);
              break;
-         case '/' :
+             case '/' :
              return divide(firstNumb, secundNumb);
              break;
-         default:
-             console.log("Something wrong!");
+             default:
+                 console.log("Something wrong!");
      }
  }
-//click button puts given number on display
-numb.forEach((num) => {
+ //click button puts given number on display
+ numb.forEach((num) => {
     document.querySelector(`#${num.id}`).addEventListener("click", (f) => {
         value += num.textContent
         updateDisplay();
     })
 });
 
+operators.forEach((op) => {
+    document.querySelector(`#${op.id}`).addEventListener("click", (f) => {
+        if(operator !== undefined) {
+            secundNumb = parseFloat(value);
+            console.log(firstNumb, operator, secundNumb)
+            
+            value = operate(firstNumb, secundNumb, operator);
+            updateDisplay();
+            firstNumb = value;
+            // operator = `"${operators[this].textContent}"`;
+            operator = op.innerHTML;
+            console.log('op', operator)
+            value = 0;   
+        }    
+        else {
+            console.log(firstNumb, operator, secundNumb)
+            firstNumb = parseFloat(value);
+            // operator = this.textContent;
+            operator = op.innerHTML;
+            value = 0;
+        }
+        
+    })
+})
+
+
 function updateDisplay() {
-     
+    
     let stringValue = parseFloat(value).toString();
     let decimalIndex = stringValue.indexOf(".");
     let length = stringValue.slice(decimalIndex).length;
@@ -63,80 +89,10 @@ function updateDisplay() {
         paraLow.textContent = parseFloat(stringValue).toFixed(3);
     }
     else {
-    paraLow.textContent = parseFloat(value);
-}}
+        paraLow.textContent = parseFloat(value);
+    }}
+    
 
-plus.addEventListener("click", (e) => {
-    if(operator !== undefined) {
-        secundNumb = parseFloat(value);
-        console.log(firstNumb, operator, secundNumb)
-        
-        value = operate(firstNumb, secundNumb, operator);
-        updateDisplay();
-        firstNumb = value;
-        operator = '+';
-        value = 0;   
-    }    
-    else {
-        firstNumb = parseFloat(value);
-        operator = '+';
-        value = 0;
-    }
-});
-
-minus.addEventListener("click", (e) => {
-    if(operator !== undefined) {
-        secundNumb = parseFloat(value);
-        console.log(firstNumb, operator, secundNumb)
-        
-        value = operate(firstNumb, secundNumb, operator);
-        updateDisplay();
-        firstNumb = value;
-        operator = '-';
-        value = 0;   
-    }    
-    else {
-        firstNumb = parseFloat(value);
-        operator = '-';
-        value = 0;
-    }
-});
-
-multiplyBtn.addEventListener("click", (e) => {
-    if(operator !== undefined) {
-        secundNumb = parseFloat(value);
-        console.log(firstNumb, operator, secundNumb)
-        
-        value = operate(firstNumb, secundNumb, operator);
-        updateDisplay();
-        firstNumb = value;
-        operator = '*';
-        value = 0;   
-    }    
-    else {
-        firstNumb = parseFloat(value);
-        operator = '*';
-        value = 0;
-    }
-});
-
-divideBtn.addEventListener("click", (e) => {
-    if(operator !== undefined) {
-        secundNumb = parseFloat(value);
-        console.log(firstNumb, operator, secundNumb)
-        
-        value = operate(firstNumb, secundNumb, operator);
-        updateDisplay();
-        firstNumb = value;
-        operator = '/';
-        value = 0;   
-    }    
-    else {
-        firstNumb = parseFloat(value);
-        operator = '/';
-        value = 0;
-    }
-});
 
 equal.addEventListener('click', (e) => {
     if (operator !== undefined){
